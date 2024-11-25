dfcddb4fd41c1b34c5797e4f9e6cc72f8beb2d81
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-11-25 11:22:02 -0500

    Remove unused function

--- src/util.js
+++ src/util.js
@@ -7,29 +7,16 @@
 // Type Definitions
 //-----------------------------------------------------------------------------
 
-/*
- * Note: Using `import()` in the JSDoc comments below because including them as
- * typedef comments here caused Rollup to remove them. I couldn't figure out why
- * this was happening so just working around for now.
- */
+/** @typedef {import("css-tree").SyntaxMatchError} SyntaxMatchError */
 
 //-----------------------------------------------------------------------------
 // Helpers
 //-----------------------------------------------------------------------------
 
 /**
- * Determines if an error is a reference error.
- * @param {Object} error The error object to check.
- * @returns {error is import("css-tree").SyntaxReferenceError} True if the error is a reference error, false if not.
- */
-export function isSyntaxReferenceError(error) {
-	return typeof error.reference === "string";
-}
-
-/**
  * Determines if an error is a syntax match error.
  * @param {Object} error The error object to check.
- * @returns {error is import("css-tree").SyntaxMatchError} True if the error is a syntax match error, false if not.
+ * @returns {error is SyntaxMatchError} True if the error is a syntax match error, false if not.
  */
 export function isSyntaxMatchError(error) {
 	return typeof error.syntax === "string";
