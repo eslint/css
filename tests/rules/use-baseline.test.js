4c981ecf7ee9a04730ce4c104a353c3a763e64b9
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-05-12 11:53:24 -0400

    feat: Update Baseline data

--- tests/rules/use-baseline.test.js
+++ tests/rules/use-baseline.test.js
@@ -91,18 +91,18 @@ ruleTester.run("use-baseline", rule, {
 	],
 	invalid: [
 		{
-			code: "a { accent-color: bar; backdrop-filter: auto }",
+			code: "a { alignment-baseline: baseline; backdrop-filter: auto }",
 			errors: [
 				{
 					messageId: "notBaselineProperty",
 					data: {
-						property: "accent-color",
+						property: "alignment-baseline",
 						availability: "widely",
 					},
 					line: 1,
 					column: 5,
 					endLine: 1,
-					endColumn: 17,
+					endColumn: 23,
 				},
 				{
 					messageId: "notBaselineProperty",
@@ -111,26 +111,26 @@ ruleTester.run("use-baseline", rule, {
 						availability: "widely",
 					},
 					line: 1,
-					column: 24,
+					column: 35,
 					endLine: 1,
-					endColumn: 39,
+					endColumn: 50,
 				},
 			],
 		},
 		{
-			code: "a { accent-color: bar; backdrop-filter: auto }",
+			code: "a { alignment-baseline: baseline; backdrop-filter: auto }",
 			options: [{ available: "newly" }],
 			errors: [
 				{
 					messageId: "notBaselineProperty",
 					data: {
-						property: "accent-color",
+						property: "alignment-baseline",
 						availability: "newly",
 					},
 					line: 1,
 					column: 5,
 					endLine: 1,
-					endColumn: 17,
+					endColumn: 23,
 				},
 			],
 		},
@@ -246,18 +246,18 @@ ruleTester.run("use-baseline", rule, {
 			],
 		},
 		{
-			code: "@supports not (accent-color: auto) { a { accent-color: auto } }",
+			code: "@supports not (alignment-baseline: baseline) { a { alignment-baseline: baseline } }",
 			errors: [
 				{
 					messageId: "notBaselineProperty",
 					data: {
-						property: "accent-color",
+						property: "alignment-baseline",
 						availability: "widely",
 					},
 					line: 1,
-					column: 42,
+					column: 52,
 					endLine: 1,
-					endColumn: 54,
+					endColumn: 70,
 				},
 			],
 		},
