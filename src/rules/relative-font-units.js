03e219f90501841b02758c870494a35ac7b64aba
Tanuj Kanti <tanujkanti4441@gmail.com>
2025-05-15 19:59:35 +0530

    feat: add relative-font-units rule

---
+++ src/rules/relative-font-units.js
@@ -0,0 +1,167 @@
+/**
+ * @fileoverview Enforce the use of relative units for font size.
+ * @author Tanuj Kanti
+ */
+
+//-----------------------------------------------------------------------------
+// Type Definitions
+//-----------------------------------------------------------------------------
+
+/**
+ * @import { CSSRuleDefinition } from "../types.js"
+ * @typedef {"allowedFontUnits"} RelativeFontUnitsMessageIds
+ * @typedef {[{allow?: string[]}]} RelativeFontUnitsOptions
+ * @typedef {CSSRuleDefinition<{ RuleOptions: RelativeFontUnitsOptions, MessageIds: RelativeFontUnitsMessageIds}>} RelativeFontUnitsRuleDefinition
+ */
+
+//-----------------------------------------------------------------------------
+// Helpers
+//-----------------------------------------------------------------------------
+
+const relativeFontUnits = [
+	"%",
+	"cap",
+	"ch",
+	"em",
+	"ex",
+	"ic",
+	"lh",
+	"rcap",
+	"rch",
+	"rem",
+	"rex",
+	"ric",
+	"rlh",
+];
+
+const fontSizeIdentifiers = new Set([
+	"xx-small",
+	"x-small",
+	"small",
+	"medium",
+	"large",
+	"x-large",
+	"xx-large",
+	"smaller",
+	"larger",
+]);
+
+//-----------------------------------------------------------------------------
+// Rule Definition
+//-----------------------------------------------------------------------------
+
+/** @type {RelativeFontUnitsRuleDefinition} */
+export default {
+	meta: {
+		type: "suggestion",
+
+		docs: {
+			description: "Enforce the use of relative font units",
+			recommended: false,
+			url: "https://github.com/eslint/css/blob/main/docs/rules/relative-font-units.md",
+		},
+
+		schema: [
+			{
+				type: "object",
+				properties: {
+					allow: {
+						type: "array",
+						items: {
+							enum: relativeFontUnits,
+							uniqueItems: true,
+						},
+					},
+				},
+			},
+		],
+
+		defaultOptions: [
+			{
+				allow: ["rem"],
+			},
+		],
+
+		messages: {
+			allowedFontUnits:
+				"Use only allowed relative units for 'font-size' - {{allowedFontUnits}}.",
+		},
+	},
+
+	create(context) {
+		const [{ allow: allowedFontUnits }] = context.options;
+
+		return {
+			Declaration(node) {
+				if (node.property === "font-size") {
+					const value = node.value.children[0];
+
+					if (
+						allowedFontUnits.includes("%") &&
+						value.type === "Percentage"
+					) {
+						return;
+					}
+
+					if (
+						!allowedFontUnits.includes(value?.unit) ||
+						value.type === "Identifier"
+					) {
+						context.report({
+							loc: value.loc,
+							messageId: "allowedFontUnits",
+							data: {
+								allowedFontUnits,
+							},
+						});
+					}
+				}
+
+				if (node.property === "font") {
+					const value = node.value;
+					const dimensionNode = value.children.find(
+						child => child.type === "Dimension",
+					);
+					const identifierNode = value.children.find(
+						child =>
+							child.type === "Identifier" &&
+							fontSizeIdentifiers.has(child.name),
+					);
+					const percentageNode = value.children.find(
+						child => child.type === "Percentage",
+					);
+					let location;
+					let shouldReport = false;
+
+					if (!allowedFontUnits.includes("%") && percentageNode) {
+						shouldReport = true;
+						location = percentageNode.loc;
+					}
+
+					if (identifierNode) {
+						shouldReport = true;
+						location = identifierNode.loc;
+					}
+
+					if (
+						dimensionNode &&
+						!allowedFontUnits.includes(dimensionNode.unit)
+					) {
+						shouldReport = true;
+						location = dimensionNode.loc;
+					}
+
+					if (shouldReport) {
+						context.report({
+							loc: location,
+							messageId: "allowedFontUnits",
+							data: {
+								allowedFontUnits,
+							},
+						});
+					}
+				}
+			},
+		};
+	},
+};
---
+++ tests/rules/relative-font-units.test.js
@@ -0,0 +1,721 @@
+/**
+ * @fileoverview Tests for relative-font-units rule.
+ * @author Tanuj Kanti
+ */
+
+//------------------------------------------------------------------------------
+// Imports
+//------------------------------------------------------------------------------
+
+import rule from "../../src/rules/relative-font-units.js";
+import css from "../../src/index.js";
+import { RuleTester } from "eslint";
+import dedent from "dedent";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+	plugins: {
+		css,
+	},
+	language: "css/css",
+});
+
+ruleTester.run("relative-font-units", rule, {
+	valid: [
+		"a { font-size: 1rem; }",
+		"a { font: 2rem Arial, sans-serif; }",
+		"a { font: 1.2rem/2 Arial, sans-serif; }",
+		{
+			code: "a { font-size: 1em; }",
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 20%; }",
+			options: [
+				{
+					allow: ["%"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2cap; }",
+			options: [
+				{
+					allow: ["cap"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 20ch; }",
+			options: [
+				{
+					allow: ["ch"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 3ex; }",
+			options: [
+				{
+					allow: ["ex"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2ic; }",
+			options: [
+				{
+					allow: ["ic"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1lh; }",
+			options: [
+				{
+					allow: ["lh"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2rcap; }",
+			options: [
+				{
+					allow: ["rcap"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 20rch; }",
+			options: [
+				{
+					allow: ["rch"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2rex; }",
+			options: [
+				{
+					allow: ["rex"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1.5ric; }",
+			options: [
+				{
+					allow: ["ric"],
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1rlh; }",
+			options: [
+				{
+					allow: ["rlh"],
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 1rem;
+                }
+                b {
+                    font-size: 1em;
+                }
+            `,
+			options: [
+				{
+					allow: ["rem", "em"],
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 1em;
+                    height: 20px;
+                }
+            `,
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 1em;
+                    height: 20px;
+                }
+                b {
+                    font-size: 1rem;
+                }
+            `,
+			options: [
+				{
+					allow: ["rem", "em"],
+				},
+			],
+		},
+		{
+			code: "a { font: 20% Arial, sans-serif; }",
+			options: [
+				{
+					allow: ["%"],
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 1rem;
+                    height: 20px;
+                }
+                b {
+                    font: 2em Arial, sans-serif;
+                }
+            `,
+			options: [
+				{
+					allow: ["rem", "em"],
+				},
+			],
+		},
+	],
+	invalid: [
+		{
+			code: "a { font-size: 1px; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1em; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1rem; }",
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 20%; }",
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2em; }",
+			options: [
+				{
+					allow: ["%"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 20ch; }",
+			options: [
+				{
+					allow: ["cap"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2cap; }",
+			options: [
+				{
+					allow: ["ch"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 10px; }",
+			options: [
+				{
+					allow: ["ex"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2rem; }",
+			options: [
+				{
+					allow: ["ic"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2ic; }",
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1rlh; }",
+			options: [
+				{
+					allow: ["lh"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1lh; }",
+			options: [
+				{
+					allow: ["rcap"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2in; }",
+			options: [
+				{
+					allow: ["rch"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 3ex; }",
+			options: [
+				{
+					allow: ["rex"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 2rem; }",
+			options: [
+				{
+					allow: ["ric"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: 1lh; }",
+			options: [
+				{
+					allow: ["rlh"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 1rem;
+                }
+                b {
+                    font-size: 1em;
+                }
+            `,
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 10px;
+                    height: 3em;
+                }
+            `,
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: xx-small; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: xx-small Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: x-small; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: x-small Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: small; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: small Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: medium; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: medium Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: large; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: large Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: x-large; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: x-large Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: xx-large; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: xx-large Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: smaller; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: smaller Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font-size: larger; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: "a { font: larger Arial, sans-serif; }",
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font: 2rem Arial, sans-serif;
+                }
+            `,
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font: 20% Arial, sans-serif;
+                }
+            `,
+			options: [
+				{
+					allow: ["em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font: 2em Arial, sans-serif;
+                }
+            `,
+			options: [
+				{
+					allow: ["%"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font: small Arial, sans-serif;
+                }
+            `,
+			options: [
+				{
+					allow: ["%"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 2rem;
+                }
+            `,
+			options: [
+				{
+					allow: ["%", "em"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 2rem;
+                }
+                b {
+                    font-size: 2em;
+                }
+            `,
+			options: [
+				{
+					allow: ["rem", "rex"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font-size: 2rem;
+                    height: 50%;
+                }
+                b {
+                    font-size: 2em;
+                }
+            `,
+			options: [
+				{
+					allow: ["rem", "rex", "%"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+		{
+			code: dedent`
+                a {
+                    font: italic bold 2rem "Helvetica", sans-serif;
+                }
+            `,
+			options: [
+				{
+					allow: ["lh", "rex", "%"],
+				},
+			],
+			errors: [
+				{
+					messageId: "allowedFontUnits",
+				},
+			],
+		},
+	],
+});
