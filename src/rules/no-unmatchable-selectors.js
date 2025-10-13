f7f18c0d5f332f0e630dcfc5fcad5dbfa2efe965
TKDev7 <tkdev7@tutamail.com>
2025-10-14 02:57:09 +0300

    feat: add `no-unmatchable-selectors` rule

---
+++ src/rules/no-unmatchable-selectors.js
@@ -0,0 +1,59 @@
+/**
+ * @fileoverview Rule to disallow unmatchable selectors.
+ * @author TKDev7
+ */
+
+//-----------------------------------------------------------------------------
+// Type Definitions
+//-----------------------------------------------------------------------------
+
+/**
+ * @import { CSSRuleDefinition } from "../types.js"
+ * @typedef {"unmatchableSelector"} NoUnmatchableSelectorsMessageIds
+ * @typedef {CSSRuleDefinition<{ RuleOptions: [], MessageIds: NoUnmatchableSelectorsMessageIds }>} NoUnmatchableSelectorsRuleDefinition
+ */
+
+//-----------------------------------------------------------------------------
+// Rule Definition
+//-----------------------------------------------------------------------------
+
+/** @type {NoUnmatchableSelectorsRuleDefinition} */
+export default {
+	meta: {
+		type: "problem",
+
+		docs: {
+			description: "Disallow unmatchable selectors",
+			recommended: true,
+			url: "https://github.com/eslint/css/blob/main/docs/rules/no-unmatchable-selectors.md",
+		},
+
+		messages: {
+			unmatchableSelector:
+				"Unexpected unmatchable selector '{{selector}}'.",
+		},
+	},
+
+	create(context) {
+		const { sourceCode } = context;
+
+		return {
+			AnPlusB(node) {
+				const a = Number(node.a);
+				const b = Number(node.b);
+
+				if (a <= 0 && b <= 0) {
+					const pseudo = sourceCode.getParent(
+						sourceCode.getParent(node),
+					);
+
+					context.report({
+						loc: pseudo.loc,
+						messageId: "unmatchableSelector",
+						data: { selector: sourceCode.getText(pseudo) },
+					});
+				}
+			},
+		};
+	},
+};
