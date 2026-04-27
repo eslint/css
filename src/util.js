aed097e068fabbeb17eb179ee9a4295800124471
루밀LuMir <rpfos@naver.com>
2026-04-27 19:38:38 +0900

    wip

--- src/util.js
+++ src/util.js
@@ -17,18 +17,18 @@
 
 /**
  * Determines if an error is a syntax match error.
- * @param {SyntaxMatchError | SyntaxReferenceError | Error} error The error object to check.
+ * @param {Object} error The error object to check.
  * @returns {error is SyntaxMatchError} True if the error is a syntax match error, false if not.
  */
 export function isSyntaxMatchError(error) {
-	return "syntax" in error && typeof error.syntax === "string";
+	return typeof error.syntax === "string";
 }
 
 /**
  * Determines if an error is a syntax reference error.
- * @param {SyntaxMatchError | SyntaxReferenceError | Error} error The error object to check.
+ * @param {Object} error The error object to check.
  * @returns {error is SyntaxReferenceError} True if the error is a syntax reference error, false if not.
  */
 export function isSyntaxReferenceError(error) {
-	return "reference" in error && typeof error.reference === "string";
+	return typeof error.reference === "string";
 }
